4909ae16dc560aba32894017d332d95286c70072
JJJDataGuru <jjjacques@gmail.com>
2019-02-09 23:16:47 -0500

    Source code update

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,8 +10,6 @@ with open(csvpath, newline='') as budget_csv:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(budget_csv, delimiter=',')
 
-    #print(csvreader)
-
     # Read the header row first (skip this step if there is now header)
     budget_header = next(csvreader)
     
@@ -22,19 +20,37 @@ with open(csvpath, newline='') as budget_csv:
     total_month = 0
     total_sum = 0
     avg_chg = 0
-    row_count = 0
+    num_chg = 0
+    row_count = 0 
+
+  
+
 
     for row in csvreader:
         total_month = total_month + 1
  
         total_sum = int(row[1]) + total_sum
-       
+
+        def average(numbers):
+            length = len(numbers)
+            total = 0.0
+            for number in numbers:
+                total = total + number
+            return total / length
+
+      #  num_chg =  int(row[1])  + num_chg
+      #  avg_chg = num_chg / total_month
+
+      #  print(row[1])
+
+    print(average(int(row[1]))  
     print("Financial Analysis")
     print("----------------------------------")
     print(f"Total Months: {total_month}")
     print(f"Total: {total_sum}")
-  #  print(f"Average Change: {avg_chg}")
-    #print(f"length: {length}")
+    print(f"Average Change: {avg_chg}")
+    print(f"Greatest Increase in Profits: {month} + ({amount})")
+    print(f"Greatest Decrease in Profits: {month} + ({amount})")
     
 
 
